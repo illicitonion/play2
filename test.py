6151f6715d8c8824c653ae7cbb818c1787f09760
Daniel Wagner-Hall <dawagner@gmail.com>
2012-09-09 16:25:14 +0100

    Adding chromedriver

--- test.py
+++ test.py
@@ -3,6 +3,6 @@ from selenium import webdriver
 
 class SmokeTest(unittest.TestCase):
     def test_can_start_browser(self):
-        driver = webdriver.Chrome()
+        driver = webdriver.Chrome(executable_path='chromedriver_linux32')
         driver.get('http://www.google.com')
         self.assertEquals('Google', driver.title)
