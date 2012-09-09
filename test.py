922180f4403fcd9d75a173357508c624a4e52df7
Daniel Wagner-Hall <dawagner@gmail.com>
2012-09-09 16:34:24 +0100

    Try specifying an absolute path

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
+import os
 import unittest
 from selenium import webdriver
 
 class SmokeTest(unittest.TestCase):
     def test_can_start_browser(self):
-        driver = webdriver.Chrome(executable_path='chromedriver_linux32')
+        driver = webdriver.Chrome(executable_path=os.path.abspath('./chromedriver_linux32'))
         driver.get('http://www.google.com')
         self.assertEquals('Google', driver.title)
